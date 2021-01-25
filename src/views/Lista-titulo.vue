df2891120dc75ddd9ad807a7af0b61ddefa5333b
SariMelina <mmkings_beba@hotmail.com>
2021-01-25 03:17:15 -0600

    Adding a file to the view folder that calls all the components in the components folder

---
+++ src/views/Lista-titulo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="bg-light">
+      <Lista titulo="Lista de Tareas"/>
+      <Nueva-Tarea :tareas="tareas"/>
+      <Crear-Lista :tareas="tareas"/>
+    </div>
+  </div>  
+</template>
+
+<script>
+// @ is an alias to /src
+import Lista from "../components/Lista.component";
+import NuevaTarea from '../components/NuevaTarea.component';
+import CrearLista from '../components/CrearLista.component';
+
+
+export default {
+  name: "ListaComponent",
+  components: {
+    Lista,
+    NuevaTarea,
+    CrearLista
+  },
+  data () {
+    return {
+      tareas : [
+        {
+          texto: 'Aprender Vue.js',
+          terminada: false
+        },
+        {
+          texto: 'Aprender Ionic',
+          terminada: false
+        },
+        {
+          texto: 'Aprender Angular 2',
+          terminada: false
+        }
+      ]
+    }    
+  }
+};
+</script>
