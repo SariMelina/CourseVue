1dcae829546719368094c4fc14771a18b4a86720
SariMelina <mmkings_beba@hotmail.com>
2021-01-25 03:13:04 -0600

    Adding a  component that shows all tasks in the task list and adding the method to delete each task

---
+++ src/components/CrearLista.component.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul class="list-group">
+        <li v-for="(item, index) in tareas" :key="item.texto" class="list-group-item"
+        v-bind:class="{terminada:item.terminada}">
+            {{item.texto}}
+            <span class="pull-right">
+                <button type="button" 
+                class="btn btn-success btn-xs glyphicon glyphicon-ok"
+                @click="item.terminada = !item.terminada"
+                ></button>
+                
+                <button type="button" 
+                class="btn btn-danger btn-xs glyphicon glyphicon-erase"
+                @click="borrarTarea(index)"
+                ></button>                    
+            </span>             
+                        
+        </li>                
+    </ul>
+</template>
+
+<script>
+
+export default {
+  props: ['tareas'],
+  methods: {
+      borrarTarea (index) {
+          this.tareas.splice(index,1)
+      }
+  }
+};
+</script>
+
+<style>
+    .terminada {
+        color: gray;
+        text-decoration: line-through;
+    }
+</style>
