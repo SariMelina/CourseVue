b980ee1ea648f69f4e69a12d0076b419694d1bc4
SariMelina <mmkings_beba@hotmail.com>
2021-01-25 03:21:31 -0600

    Adding a component that shows the title which is set in the 'Lista.component' file

---
+++ src/components/Lista.component.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="bg-light">          
+            <div>
+                <h2>{{ titulo }}</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Lista",
+  props: ['titulo'],
+};
+</script>
